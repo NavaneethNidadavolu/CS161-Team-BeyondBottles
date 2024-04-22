0dd2ee30f7ea61cae78b852dc9feb584bf2ec1d3
Navaneeth Nivol <33155848+navaneethnivol@users.noreply.github.com>
2024-04-22 16:39:26 -0700

    added login screen

---
+++ dev/frontend/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+
+export default function Dashboard() {
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-around p-24">
+            Dashboard
+        </main>
+    );
+}
